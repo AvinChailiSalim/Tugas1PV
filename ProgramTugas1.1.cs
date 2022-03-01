c345ca17bcf50f2d05b7015115b6f33a0413d75c
AvinChailiSalim <86093166+AvinChailiSalim@users.noreply.github.com>
2022-03-01 13:48:58 +0700

    Program Commit

---
+++ ProgramTugas1.1.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: XTS
+ * Date: 3/1/2022
+ * Time: 10:11 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Tugas1._
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ ProgramTugas1.2.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: XTS
+ * Date: 3/1/2022
+ * Time: 10:29 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Tugas1._
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Nama kamu siapa?");
+			
+			string nama = Console.ReadLine();
+			Console.WriteLine("Nama kamu "+nama);
+		
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
